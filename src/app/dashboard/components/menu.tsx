5ead54d43e1338ea86c75eeae3769921783a5612
major101x <72233326+major101x@users.noreply.github.com>
2026-02-17 11:50:05 +0100

    implement pull request fetching

---
+++ src/app/dashboard/components/menu.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useActionState } from "react";
+import { fetchPR } from "@/app/lib/actions";
+import Link from "next/link";
+
+export function Menu({
+  handleInstallClick,
+}: {
+  handleInstallClick: () => void;
+}) {
+  const [prState, fetchPRAction] = useActionState(fetchPR, null);
+  return (
+    <>
+      <h1>Welcome to Diffy!</h1>
+      <p>Please install the app to get started.</p>
+      <Link href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/github/install`}>
+        Install Diffy
+      </Link>
+
+      <form action={fetchPRAction}>
+        <input
+          type="text"
+          name="prUrl"
+          placeholder="Type the pull request url here"
+        />
+        <button>Analyze PR</button>
+      </form>
+      {prState?.pr && <pre>{JSON.stringify(prState.pr, null, 2)}</pre>}
+      {prState?.diff && <pre>{prState.diff}</pre>}
+    </>
+  );
+}
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,6 +1,12 @@
+"use server";
 import { cookies } from "next/headers";
 
-export async function fetchData(url: string) {
+type FetchPRState = {
+  pr: Record<string, unknown> | null;
+  diff: string | null;
+};
+
+export async function fetchData(url: string, type: "json" | "text" = "json") {
   const cookieStore = await cookies();
   const token = cookieStore.get("token");
 
@@ -14,10 +20,44 @@ export async function fetchData(url: string) {
     },
   });
 
+  console.log(response);
   if (!response.ok) {
     throw new Error("Failed to fetch data on the server");
   }
 
-  const data = await response.json();
+  if (type === "json") {
+    const data = await response.json();
+    return data;
+  }
+  const data = await response.text();
+
   return data;
+}
+
+export async function fetchPR(
+  prevState: FetchPRState | null,
+  formData: FormData,
+): Promise<FetchPRState> {
+  const prUrl = formData.get("prUrl")?.toString();
+  const regex = /https:\/\/www\.github\.com\/([^/]+)\/([^/]+)\/pull\/([\d]+)/;
+  const match = prUrl?.match(regex);
+  if (!match) {
+    throw new Error("Invalid pull request URL");
+  }
+  const owner = match[1];
+  const repo = match[2];
+  const pull_number = Number(match[3]);
+  const prAndDiff = Promise.all([
+    fetchData(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/github/pull-request/${owner}/${repo}/${pull_number}`,
+    ),
+    fetchData(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/github/diff/${owner}/${repo}/${pull_number}`,
+      "text",
+    ),
+  ]);
+  const [pullRequest, diff] = await prAndDiff;
+  console.log(pullRequest);
+  console.log(diff);
+  return { pr: pullRequest, diff };
 }
